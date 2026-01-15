9e38525ea3e4dd33c4b7af5fc7fc9c9d14f223ef
paramjeetkaurdhanjal8 <paramjeetkaurdhanjal8@gmail.com>
2026-01-15 16:01:41 +0530

    modified everia.py

--- reveria.py
+++ reveria.py
@@ -394,13 +394,6 @@ def comment(post_id):
     db.session.commit()
     return redirect(request.referrer)
 
-@app.route('/delete/<int:post_id>', methods=['POST'])
-def delete_post(post_id):
-    post = Post.query.get_or_404(post_id)
-    if post.author.username == session['username']:
-        db.session.delete(post)
-        db.session.commit()
-    return redirect(url_for('home'))
 
 
 @app.route('/unfollow/<int:user_id>', methods=['POST'])
